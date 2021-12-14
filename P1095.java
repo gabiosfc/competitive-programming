707323a6c7a669b690c460534895612bd6737392
gabiosfc <gabiosfc@gmail.com>
2021-12-14 15:54:43 -0300

    Resolução do problema P1095 (while)

---
+++ P1095.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+public class P1095 {
+ 
+    public static void main (String[] args) throws IOException {
+      
+        int I = 1;
+        int J = 60;
+
+        while (J >= 0){
+            System.out.println("I="+I + " J="+J);
+            J = J-5;
+            I = I+3;
+
+        }
+
+    }
+ 
+}
+        
+        
